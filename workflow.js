5ab3d8a59de7e8b5cab56572c4e8998cf4f55873
tminh25 <minhsip2510@gmail.com>
2023-03-21 09:04:03 +0700

    update send mail when no drawing received

--- workflow.js
+++ workflow.js
@@ -121,33 +121,21 @@ export default async (payload, secretList, autobotCode, autobotSecret) => {
     const todayExcelFile = await gnets.getTodayExcel(todayTempDirectory);
 
     if (!todayExcelFile) {
-      autoBotDebugger("No unreceived drawings found!")
+      autoBotDebugger("No unreceived drawings found!");
+
+      await sendMail(SMTPConfig, mailTo, {
+        mailSubject: `Tổng hợp bản vẽ của ngày ${nowUserDateFormatted}`,
+        mailBody: `Hệ thống autobot xin gửi lại bạn danh sách bản vẽ của ngày ${nowUserDateFormatted}, vào lúc ${nowUserTimeFormatted}. <br/><br/>
+        Ngày hôm nay GNetS không nhận được bản vẽ nào!`,
+        attachments,
+      });
       return { data: {} };
     }
     let drawingList, mergeRows;
     if (todayExcelFile) {
       ({ drawing: drawingList, mergeRows } = getTodayExcelData(path.resolve(todayTempDirectory, todayExcelFile)));
-      drawingList = [
-        { inhouseDc: "MP23-00266-006", customerDc: "", aKeyNo: "4SW18954B2", pKeyNo: "A33555-12", dwgNo: "39115-T2A -A610-M1", dwgName: "JACK ASSY,AUX (W USB)" },
-        { inhouseDc: "MP23-00266-010", customerDc: "", aKeyNo: "4SW20754A3", pKeyNo: "A36212-03", dwgNo: "39115-TG7 -A410-M1-A  ", dwgName: "JACK ASSY,ACC&USB&AUX(NH-167L)" },
-        { inhouseDc: "MP23-00309-001", customerDc: "JMCN-00106", aKeyNo: "3CL01619A4", pKeyNo: "A20990-04", dwgNo: "30500-ZW2 -F010-M1", dwgName: "COIL ASSY,IGN" },
-        { inhouseDc: "MP23-00206", customerDc: "", aKeyNo: "E16937", pKeyNo: "E16937", dwgNo: "E16937", dwgName: "LED-CSL0901VT1(C)" },
-        { inhouseDc: "MP23-00309-002", customerDc: "JMCN-00106", aKeyNo: "3CL01078A4", pKeyNo: "A06574-04", dwgNo: "30511-ZV7 -0030", dwgName: "COIL 1 COMP,IGN" },
-        { inhouseDc: "MP23-00309-003", customerDc: "JMCN-00106", aKeyNo: "3CL01079A3", pKeyNo: "A06575-03", dwgNo: "30512ZV7 0030 ", dwgName: "COIL 2 COMP,IGN" },
-        { inhouseDc: "MP23-00309-004", customerDc: "JMCN-00106", aKeyNo: "3CL01618A2", pKeyNo: "A20989-02", dwgNo: "30513-ZW2 -F010-M1", dwgName: "COIL 3 COMP,IGN" },
-        { inhouseDc: "MP23-00309-005", customerDc: "JMCN-00106", aKeyNo: "3OT04867", pKeyNo: "F11365", dwgNo: "30700-ZV5 -0030", dwgName: "CAP ASSY,NOISE SUPPRESSOR" },
-        { inhouseDc: "MP20-01647-003", customerDc: "", aKeyNo: "2DC03086A7", pKeyNo: "A19140-07", dwgNo: "5316A-GEY -7500", dwgName: "HOUSING ASSY UND THROT" },
-        { inhouseDc: "MP20-01647-004", customerDc: "", aKeyNo: "2DC03489A2", pKeyNo: "A30088-02", dwgNo: "5316A-KWV -0000", dwgName: "HOUSING ASSY,UND THROT" },
-        { inhouseDc: "MP23-00349-001", customerDc: "", aKeyNo: "4OT10539D8", pKeyNo: "A17188-28", dwgNo: "28810-PPW -0130", dwgName: "PICK UP ASSY" },
-        { inhouseDc: "MP23-00349-002", customerDc: "", aKeyNo: "4OT10538E0", pKeyNo: "A17189-30", dwgNo: "28820-PPW -0130", dwgName: "PICK UP ASSY" },
-        { inhouseDc: "MP23-00349-003", customerDc: "", aKeyNo: "4OT10971C3", pKeyNo: "A18858-23", dwgNo: "28810-RER -0040", dwgName: "PICK UP ASSY" },
-        { inhouseDc: "MP23-00349-004", customerDc: "", aKeyNo: "4OT11429C2", pKeyNo: "A22600-22", dwgNo: "28810-RPC -0130", dwgName: "PICK UP ASSY" },
-        { inhouseDc: "MP23-00349-005", customerDc: "", aKeyNo: "4OT11516B9", pKeyNo: "A22661-19", dwgNo: "28820-RPC -0130", dwgName: "PICK UP ASSY" },
-        { inhouseDc: "MP23-00349-006", customerDc: "", aKeyNo: "4OT11799B6", pKeyNo: "A24569-16", dwgNo: "28820-R29 -0130", dwgName: "PICK UP ASSY" },
-        { inhouseDc: "MP23-00349-007", customerDc: "", aKeyNo: "4OT11944B6", pKeyNo: "A25583-16", dwgNo: "28810-R90 -0130", dwgName: "PICK UP ASSY" },
-        { inhouseDc: "MP23-00349-008", customerDc: "", aKeyNo: "4OT12963B3", pKeyNo: "A31665-13", dwgNo: "28820-RJ2 -0030", dwgName: "PICK UP ASSY" },
-      ];
-      mergeRows = [];
+      // drawingList = [];
+      // mergeRows = [];
     }
 
     skippedDrawings = uniqBy(Array.from(drawingList), 'inhouseDc')
